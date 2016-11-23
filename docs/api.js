6849a0f472fedfd0bda825901ede3c3433c46267
Richard Walker <digitalsadhu@gmail.com>
2016-11-23 20:23:52 +0100

    Add docs

---
+++ docs/api.js
@@ -0,0 +1,21 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Application",
+        "Base",
+        "ErrorHandler",
+        "Http",
+        "Initializer",
+        "Inject",
+        "Middleware",
+        "MiddlewareRouter",
+        "Mixin",
+        "Route",
+        "Router",
+        "Service"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
