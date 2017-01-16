84c80a20462ed4d1cba5746b9e09550210fd8275
Mark Stuart <mstuart_is@me.com>
2017-01-16 21:18:51 +1300

    Initial implementation of QueryFilter w/ tests

--- src/classes/query-builder.js
+++ src/classes/query-builder.js
@@ -1,89 +1,133 @@
 module.exports = class QueryFilter {
-  constructor (knex) {
-    this.knex = knex
+  constructor (knex, Model) {
+    this.Model = Model
+    this.queryBuilder = knex.queryBuilder()
+    this.depth = 0
   }
 
-  buildFilter (Model, filter) {
-    // for (const key of Object.keys(filter)) {
-    //   const value = filter[key]
-    //   if (key[0] === '$') {
-    //     // its an operator
-    //     const operator = key
-    //     switch (operator) {
-    //       case '$and':
-    //         this.knex.where(function () {
+  _orBuilder (array) {
+    const queryFilter = this
+    this.queryBuilder.where(function () {
+      queryFilter.queryBuilder = this
+      for (const value of array) {
+        const orWhere = value !== array[0]
+        queryFilter._buildQuery(value, null, orWhere)
+      }
+    })
+  }
 
-    //         })
-    //         break
-    //       case '$or':
-    //         this.knex.orWhere(function () {
+  _addClause (action, args, orWhere) {
+    if (orWhere) action = 'or' + action.charAt(0).toUpperCase() + action.slice(1)
+    this.queryBuilder = this.queryBuilder[action](...args)
+  }
 
-    //         })
-    //         break
-    //       case '$gt':
-    //         this.knex = this.knex.where(key, '>', value)
-    //         break
-    //       case '$gte':
-    //         this.knex = this.knex.where(key, '>=', value)
-    //         break
-    //       case '$lt':
-    //         this.knex = this.knex.where(key, '<', value)
-    //         break
-    //       case '$lte':
-    //         this.knex = this.knex.where(key, '<=', value)
-    //         break
-    //       case '$ne':
-    //         this.knex = this.knex.where(key, '!=', value)
-    //         break
-    //       case '$not':
-    //         this.knex = this.knex.whereNot(key, value)
-    //         break
-    //       case '$between':
-    //         this.knex = this.knex.whereBetween(key, value)
-    //         break
-    //       case '$notBetween':
-    //         this.knex = this.knex.whereNotBetween(key, value)
-    //         break
-    //       case '$in':
-    //         this.knex = this.knex.whereIn(key, value)
-    //         break
-    //       case '$notIn':
-    //         this.knex = this.knex.whereNotIn(key, value)
-    //         break
-    //       case '$like':
-    //         this.knex = this.knex.where(key, 'LIKE', value)
-    //         break
-    //       case '$notLike':
-    //         this.knex = this.knex.where(key, 'NOT LIKE', value)
-    //         break
-    //       case '$iLike':
-    //         this.knex = this.knex.where(key, 'ILIKE', value)
-    //         break
-    //       case '$notILike':
-    //         this.knex = this.knex.where(key, 'NOT ILIKE', value)
-    //         break
-    //       case '$overlap':
-    //         this.knex = this.knex.where(key, '&&', value)
-    //         break
-    //       case '$contains':
-    //         this.knex = this.knex.where(key, '@>', value)
-    //         break
-    //       case '$contained':
-    //         this.knex = this.knex.where(key, '<@', value)
-    //         break
-    //     }
-    //   } else {
-    //     // its not an operator so it is a column name
-    //     // verify its valid via Model.attributes
-    //     if (Model.attributes[key]) {
-    //       // if its not an object then this is a simple AND operation
-    //       if (typeof value !== 'object') {
-    //         this.knex = this.knex.where(key, value)
-    //       } else {
-    //         //
-    //       }
-    //     }
-    //   }
-    // }
+  _operatorFilter (colName, operator, value, orWhere) {
+    switch (operator) {
+      case '$or':
+        this._orBuilder(value)
+        break
+      case '$gt':
+        this._addClause('where', [colName, '>', value], orWhere)
+        break
+      case '$gte':
+        this._addClause('where', [colName, '>=', value], orWhere)
+        break
+      case '$lt':
+        this._addClause('where', [colName, '<', value], orWhere)
+        break
+      case '$lte':
+        this._addClause('where', [colName, '<=', value], orWhere)
+        break
+      case '$ne':
+        this._addClause('where', [colName, '!=', value], orWhere)
+        break
+      case '$not':
+        this._addClause('whereNot', [colName, value], orWhere)
+        break
+      case '$between':
+        this._addClause('whereBetween', [colName, value], orWhere)
+        break
+      case '$notBetween':
+        this._addClause('whereNotBetween', [colName, value], orWhere)
+        break
+      case '$in':
+        this._addClause('whereIn', [colName, value], orWhere)
+        break
+      case '$notIn':
+        this._addClause('whereNotIn', [colName, value], orWhere)
+        break
+      case '$like':
+        this._addClause('where', [colName, 'LIKE', value], orWhere)
+        break
+      case '$notLike':
+        this._addClause('where', [colName, 'NOT LIKE', value], orWhere)
+        break
+      case '$iLike':
+        this._addClause('where', [colName, 'ILIKE', value], orWhere)
+        break
+      case '$notILike':
+        this._addClause('where', [colName, 'NOT ILIKE', value], orWhere)
+        break
+      case '$overlap':
+        this._addClause('where', [colName, '&&', value], orWhere)
+        break
+      case '$contains':
+        this._addClause('where', [colName, '@>', value], orWhere)
+        break
+      case '$contained':
+        this._addClause('where', [colName, '<@', value], orWhere)
+        break
+      // default should throw error 'Unknown operator'
+    }
+  }
+
+  _buildQuery (filter, colName, orWhere) {
+    this.depth++
+    const filterKeys = Object.keys(filter)
+    console.log(filter, colName, orWhere)
+    if (this.depth > 1 && filterKeys.length > 1) {
+      this._createNewContext(filter, colName, orWhere)
+    } else {
+      this._handleFilter(filter, colName, orWhere)
+    }
+  }
+
+  _createNewContext (filter, colName, orWhere) {
+    const queryFilter = this
+    const action = orWhere ? 'orWhere' : 'where'
+    queryFilter.queryBuilder[action](function () {
+      queryFilter.queryBuilder = this
+      queryFilter._handleFilter(filter, colName, orWhere)
+    })
+  }
+
+  _handleFilter (filter, colName, orWhere) {
+    const queryFilter = this
+    for (const key of Object.keys(filter)) {
+      const value = filter[key]
+      if (key[0] === '$') {
+        // its an operator
+        const operator = key
+        queryFilter._operatorFilter(colName, operator, value, orWhere)
+      } else {
+        // its not an operator so it is a column name
+        // verify its valid via Model.attributes
+        if (!queryFilter.Model.attributes[key]) return // TODO: throw an error to show that the column name is invalid
+
+        // if its not an object then this is a simple AND operation
+        if (typeof value !== 'object') {
+          queryFilter._addClause('where', [key, value], orWhere)
+        } else {
+          // value is actually a filter, and possibly need the key for operator case
+          queryFilter._buildQuery(value, key, orWhere)
+        }
+      }
+    }
+  }
+
+  buildFilter (filter) {
+    this._buildQuery(filter)
+
+    return this.queryBuilder.toString().replace('select * where ', '')
   }
 }
--- test/query-builder.spec.js
+++ test/query-builder.spec.js
@@ -31,18 +31,95 @@ describe('query-filter', () => {
     }
   })
 
-  test('{id: 1} should generate id = 1', async () => {
+  test('{id: 1} should generate "id = 1"', async () => {
     // Given
     const filter = {id: 1}
     await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
 
     // When
-    const builder = new QueryBuilder(db.knex)
-    const result = await builder.buildFilter(models.Post, filter)
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
 
     // Then
-    expect(result.length).toBe(1)
-    expect(result[0].title).toBe('My Post 1')
+    expect(result).toBe('\"id\" = 1')
+  })
+
+  test('{id: {$gt: 1}} should generate "id > 1"', async () => {
+    // Given
+    const filter = {id: {$gt: 1}}
+    await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
+
+    // When
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
+
+    // Then
+    expect(result).toBe('\"id\" > 1')
+  })
+
+  test('{id: {$gt: 1}, title: "Post 2"} should generate "id > 1 AND title = \'Post 2\'"', async () => {
+    // Given
+    const filter = {id: {$gt: 1}, title: "Post 2"}
+    await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
+
+    // When
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
+
+    // Then
+    expect(result).toBe('\"id\" > 1 and \"title\" = \'Post 2\'')
+  })
+
+  test('{id: {$gt: 1}, title: {$iLike: "%OsT 2"}} should generate "id > 1 AND title ILIKE \'%OsT 2\'"', async () => {
+    // Given
+    const filter = {id: {$gt: 1}, title: {$iLike: "%OsT 2"}}
+    await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
+
+    // When
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
+
+    // Then
+    expect(result).toBe('\"id\" > 1 and \"title\" ILIKE \'%OsT 2\'')
+  })
+
+  test('{$or: [{id: 1}, {title: "Post 2"}]} should generate "id = 1 OR title = \"Post 2\""', async () => {
+    // Given
+    const filter = {$or: [{id: 1}, {title: "Post 2"}]}
+    await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
+
+    // When
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
+
+    // Then
+    expect(result).toBe('(\"id\" = 1 or \"title\" = \'Post 2\')')
+  })
+
+  test('{id: {$gt: 10}, $or: [{title: "hello"}, {title: {$like: "%bye%"}}]} should generate "id > 10 AND (title = \'hello\' OR title LIKE \'%bye%\')"', async () => {
+    // Given
+    const filter = {id: {$gt: 10}, $or: [{title: "hello"}, {title: {$like: "%bye%"}}]}
+    await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
+
+    // When
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
+
+    // Then
+    expect(result).toBe('\"id\" > 10 and (\"title\" = \'hello\' or \"title\" LIKE \'%bye%\')')
+  })
+
+  test('{id: {$gt: 10}, $or: [{title: "hello"}, {title: {$ilike: "%bye%"}, description: {$ilike: "%bye%"}}]} should generate "id > 10 AND (title = \'hello\' OR (title ILIKE \'%bye%\' AND description ILIKE \'%bye%\'))"', async () => {
+    // Given
+    const filter = {id: {$gt: 10}, $or: [{title: 'hello'}, {title: {$ilike: '%bye%'}, description: {$ilike: '%bye%'}}]}
+    await db.knex('posts').insert([{title: 'My Post 1'}, {title: 'My Post 2'}])
+
+    // When
+    const builder = new QueryBuilder(db.knex, models.Post)
+    const result = await builder.buildFilter(filter)
+
+    // Then
+    expect(result).toBe('\"id\" > 10 and (\"title\" = \'hello\' or (\"title\" ILIKE \'%bye%\' and \"description\" ILIKE \'%bye%\'))')
   })
 
   // {id: 1}
@@ -54,12 +131,17 @@ describe('query-filter', () => {
   // {id: {$gt: 1}, title: 'hello'}
   // id > 1 AND title = 'hello'
 
-  // {$or: [{id: {$gt: 1}}, {title: 'hello'} ]}
-  // id > 1 OR title = 'hello'
+  // {$or: [ {id: {$gt: 1}}, {title: 'hello', name: 'blah'} ]}
+  // id > 1 OR (title = 'hello' AND name = 'blah')
 
   // {id: {$gt: 10}, $or: [{title: 'hello'}, {title: {$like: '%bye%'}}]}
   // id > 10 AND (title = 'hello' OR title LIKE '%bye%')
 
   // {id: {$gt: 10}, $or: [{title: 'hello'}, {title: {$ilike: '%bye%'}, description: {$ilike: '%bye%'}}]}
   // id > 10 AND (title = 'hello' OR (title ILIKE '%bye%' AND description ILIKE '%bye%'))
+
+  // Notes:
+  // - drop $and syntax
+
+
 })
