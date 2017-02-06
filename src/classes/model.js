f335a74b343a7ba0817caf5535ce08e58eb6e00a
Richard Walker <digitalsadhu@gmail.com>
2017-02-06 11:06:07 +0100

    Improve model documentation

--- src/classes/model.js
+++ src/classes/model.js
@@ -20,14 +20,82 @@ module.exports = class Model extends Base {
   }
 
   /**
+   * Provides a copy of the models internal state as a plain javascript object
+   * Does not provide relationship data as models. For this you need to use
+   * individual relationship getters.
+   *
+   * @example
+   * ```
+   * const model = new PostModel({
+   *   title: 'My title',
+   *   comments: [
+   *     {comment: 'This is my comment'},
+   *     {comment: 'This is another comment'}
+   *   ]
+   * })
+   *
+   * model.comments.then(comments => {
+   *   // comments are `CommentModel` instances
+   * })
+   *
+   * // comments are plain javascript objects in an array
+   * const comments = model.attributes.comments
+   * ```
+   *
    * @return {Object} - clone of attributes hash
    */
   get attributes () {
     return JSON.parse(JSON.stringify(attributes.get(this)))
   }
 
+  /**
+   * Setter for model instances internal state.
+   * Called in model constructor with passed in state data
+   *
+   * Performs validation of incoming data and guards access to internal state.
+   * All individual getters and setters go through this setter thereby providing a single
+   * entry point for data validation and cleanup
+   *
+   * Internal state is stored as a plain object and can contain nested relationship data
+   * @example
+   * For a model
+   * ```
+   * class PostModel extends Model {
+   *   static attributes (attr) {
+   *     attr('title', 'string')
+   *   }
+   *   static relationships (relation) {
+   *     relation('hasMany', 'comment')
+   *   }
+   * }
+   * ```
+   * The following is valid though `unspecifiedProp` will be ignored:
+   * ```
+   * new PostModel({
+   *   title: 'My title',
+   *   unspecifiedProp: true,
+   *   comments: [
+   *     {comment: 'This is my comment'},
+   *     {comment: 'This is another comment'}
+   *   ]
+   * })
+   * ```
+   * The internal state of the model will be:
+   * ```
+   * {
+   *   title: 'My title',
+   *   comments: [
+   *     {comment: 'This is my comment'},
+   *     {comment: 'This is another comment'}
+   *   ]
+   * }
+   * ```
+   * @param  {Object} props - a hash of keys and values to set as the models internal data
+   */
   set attributes (props) {
     // TODO: should clone props to guard against models being passed in
+
+    // current supported types. Can be extended as needed.
     const types = {
       string: (value) => typeof value !== 'string',
       number: (value) => typeof value !== 'number',
@@ -35,7 +103,11 @@ module.exports = class Model extends Base {
       boolean: (value) => typeof value !== 'boolean'
     }
 
+    // Makes use of attributes definition defined when model is setup in the registry
     const attributeDefn = get(this, 'constructor.definition.attributes', {})
+
+    // creates the attributes hash, uses default values provided when model is defined
+    // merged with any existing stored states
     const attributesHash = {}
     Object.keys(attributeDefn).forEach(attr => {
       if (attributeDefn[attr].defaultValue) {
@@ -48,28 +120,33 @@ module.exports = class Model extends Base {
     })
     defaultsDeep(attributesHash, attributes.get(this))
 
-    // attributes
+    // whitelists props to defined attributes
+    // throws error if incorrect data type if provided but ignores
+    // any props that have not been defined
     Object.keys(attributeDefn).forEach(attr => {
       if (props[attr]) {
         const type = attributeDefn[attr].type
         if (types[type](props[attr])) {
-          throw new Error()
+          throw new Error(`Invalid property type specified. Expected ${attr} to be one of ${Object.keys(types)}, instead got ${attributeDefn[attr].type}`)
         }
         attributesHash[attr] = props[attr]
       }
     })
 
-    // relationships
+    // whitelists relationship props based on models relationships definition
+    // that was setup when model was registered
     const relationships = get(this, 'constructor.definition.relationships', {})
     Object.keys(relationships).forEach(relationship => {
       if (props[relationship]) attributesHash[relationship] = props[relationship]
     })
 
-    // primary key field
+    // if primary key field was not defined as part of the attributes whitelist
+    // implicitly define it here
     if (props[this.constructor.idField]) {
       attributesHash[this.constructor.idField] = props[this.constructor.idField]
     }
 
+    // finally set the models internal states with cleaned data
     attributes.set(this, attributesHash)
   }
 
@@ -150,25 +227,46 @@ module.exports = class Model extends Base {
    * This should be overriden to define any relationships to
    * other models that are needed.
    *
-   * Gets passed a function `relation` which should be called multiple
-   * times (once for each relationship)
+   * Gets passed a function `relation` which should be called multiple times (once for each relationship)
    * `relation` has the following signature
+   *
    * ```
-   * relation(String name, String type)
+   * relation(type, modelName, options)
    * ```
-   * Eg.
+   *
+   * `type` can be either `belongsTo` or `hasMany`
+   * `modelName` is a models `Model.modelName` property
+   * `options` is an object which may contain any or all of the keys `name`, `keyFrom` or `keyTo`
+   *
+   * @example
    * ```
-   * relation('comments', 'hasMany')
+   * relationships (relation) {
+   *   relation('hasMany', 'comment')
+   * }
    * ```
+   *
+   * @example
+   * ```
+   * relationships (relation) {
+   *   relation('hasMany', 'comment', {name: 'comments', keyFrom: 'id', keyTo: 'postId'})
+   * }
+   * ```
+   *
+   * If not specified, `name` is a pluralized version of the models name for `hasMany` or singular for `belongsTo`
+   * If not specified, `keyFrom` is the models `idField` for `hasMany` or a concatenation of `modelName` and the string 'Id' for `belongsTo`
+   * If not specified, `keyTo` is a concatenation of the related model's `modelName` and the string 'Id' for  for `hasMany` or the related models `idField` for `belongsTo`
    */
   static relationships (relation) {
     // override
   }
-  // static validation () {}
-  // get errors () {}
-  // get fields () {}
 
   /**
+   * Model name matching filename (not classname)
+   * Used throughout Ash to reference the model
+   *
+   * @example
+   *  For a model class named `MyPostModel` or `MyPostsModel`, `modelName` will be `my-post`
+   *
    * @return {string} model name
    */
   static get modelName () {
@@ -179,6 +277,12 @@ module.exports = class Model extends Base {
   }
 
   /**
+   * Table name for model of the form lowercase, pluralized with underscores separating words
+   * Calculated from models class name with `Model` stripped off the end
+   *
+   * @example
+   *  For model MyPostModel, tablename would be my_posts
+   *
    * @return {string} table name for model
    */
   static get tableName () {
@@ -188,6 +292,12 @@ module.exports = class Model extends Base {
   }
 
   /**
+   * Exposes a pluralised version of the models name
+   * Matches up with jsonapi specs `type` attribute
+   *
+   * @example
+   * for a model `post`, type would be `posts`
+   *
    * @return {string} model type
    */
   static get type () {
@@ -232,6 +342,10 @@ module.exports = class Model extends Base {
     return 'id'
   }
 
+  /**
+   * Assumes the model to be in a new state if the models id is not present in the attributes hash
+   * @return {Boolean} - true if model is considered new (not saved in database)
+   */
   get isNew () {
     return !this.attributes[this.constructor.idField]
   }
