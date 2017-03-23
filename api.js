762ad94f5267d109abd1f2f1ea9465060bc6a17c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2017-03-23 13:29:20 +0000

    Deploy ash-framework/core to github.com/ash-framework/core.git:gh-pages

---
+++ api.js
@@ -0,0 +1,37 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Adapter",
+        "Application",
+        "Ash",
+        "Base",
+        "ClassResolver",
+        "ErrorHandler",
+        "Http",
+        "Initializer",
+        "Inject",
+        "Middleware",
+        "MiddlewareRouter",
+        "Mixin",
+        "Model",
+        "QueryObjectTranslator",
+        "Registry",
+        "Route",
+        "Router",
+        "Serializer",
+        "Service",
+        "Store"
+    ],
+    "modules": [
+        "Ash"
+    ],
+    "allModules": [
+        {
+            "displayName": "Ash",
+            "name": "Ash",
+            "description": "Provides the Ash application framework"
+        }
+    ],
+    "elements": []
+} };
+});
