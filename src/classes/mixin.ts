ed5866b99363d41b38fea2ae2da29e9738a2b813
Richard Walker <digitalsadhu@gmail.com>
2017-04-05 21:16:04 +0200

    Add mixin decorator implementation

--- src/classes/mixin.ts
+++ src/classes/mixin.ts
@@ -8,3 +8,25 @@ import Base from './base'
 export default class Mixin extends Base {
 
 }
+
+export function mixin (...classes) {
+  const behaviours = new Map()
+  for (const cls of classes) {
+    for (const key of Reflect.ownKeys(cls.prototype)) {
+      behaviours.set(key, cls.prototype[key])
+    }
+  }
+
+  function _mixin (clazz) {
+    for (const [property, behaviour] of behaviours) {
+      Object.defineProperty(clazz.prototype, property, {
+        value: behaviour,
+        writable: true
+      })
+    }
+    return clazz
+  }
+
+  return _mixin
+}
+
